7020d0067b15e77013047e21b1ffa64abd5fc11c
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-03-27 20:04:30 -0300

    Identificando os botões
    botões identificados e funcionando.
    pensar agr na logística do jogo entre player 1 e 2

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-let teste = document.getElementById("ci_esq");
+// primeira fileira
+let ci_esq = document.getElementById("ci_esq");
+let ci_ce = document.getElementById("ci_ce");
+let ci_dir = document.getElementById("ci_dir");
 
-addEventListener();
+// segunda fileira
+let ce_esq = document.getElementById("ce_esq");
+let ce_ce = document.getElementById("ce_ce");
+let ce_dir = document.getElementById("ce_dir");
+
+// terceira fileira
+let ba_esq = document.getElementById("ba_esq");
+let ba_ce = document.getElementById("ba_ce");
+let ba_dir = document.getElementById("ba_dir");
+
+//player1
+ci_esq.addEventListener("click", () => (ci_esq.textContent = "X"));
+
+//player2
+ci_esq.addEventListener("click", () => (ci_esq.textContent = "O"));

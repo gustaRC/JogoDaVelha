80a918c2a3d0fa1a6e3a8d4ee3f3c51a6f7d1632
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-03-30 20:33:04 -0300

    Ideias 30/03

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let jogador = null
+let jogador = null;
 
 function jogadorAtual(j) {
-    jogador = j
+  jogador = j;
 }
 
-var elementos = document.querySelectorAll("button")
+var elementos = document.querySelectorAll("button");
 
 /* 
 vai dar certo com fé em god!!!
@@ -13,24 +13,38 @@ console.log(elementos[5].innerHTML = "X")
 */
 
 function inicio() {
-    elementos.innerHTML = ""
-    
-    
-    
+  elementos.innerHTML = "";
 }
-inicio()
+inicio();
 
 function alteraConteudo(i) {
-    elementos[i].textContent = valor()
+  elementos[i].textContent = valor();
 }
 
 function valor() {
-    if (jogador == "X") {
-        jogador = "O"
-        return "O"
-    } else {
-        jogador = "X"
-        return "X"
-    }
+  if (jogador == "X") {
+    jogador = "O";
+    return "O";
+  } else {
+    jogador = "X";
+    return "X";
+  }
 }
 
+/*
+parte de pressionar o botao:
+ao apertar muda o conteudo, puxa o indice q foi apertado
+armazena em outro array, caso contenha a combinação certa: parabens
+apos apertado o botao tambem remover o indice com as funções de array
+
+
+
+Procurar o índice de um item na Array
+array.indexOf("i")
+
+Remover um item pela posição do índice
+array.splice(i)
+
+pra designar empate:
+if (array.lenght >= 9)
+*/

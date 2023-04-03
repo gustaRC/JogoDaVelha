06bb7be7890618ef98193d1331dd7cef43f92400
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-04-03 17:26:10 -0300

    Atualização 03/04
    Falta definir um vencedor! Somente

--- script.js
+++ script.js
@@ -3,23 +3,30 @@ const elementos = document.querySelectorAll("button")
 const casas = Array.from(elementos)
 
 const combGanhar = [
-    [n1, n2, n3],
-    [n4, n5, n6],
-    [n7, n8, n9],
-    [n1, n4, n7],
-    [n2, n5, n8],
-    [n3, n6, n9],
-    [n1, n5, n9],
-    [n3, n5, n7]
+    ["n1", "n2", "n3"],
+    ["n4", "n5", "n6"],
+    ["n7", "n8", "n9"],
+    ["n1", "n4", "n7"],
+    ["n2", "n5", "n8"],
+    ["n3", "n6", "n9"],
+    ["n1", "n5", "n9"],
+    ["n3", "n5", "n7"]
 ]
+
 
 document.addEventListener("click", (el) => {
     jogoVelha(el.target.id) //target = retorna o elemento onde ocorreu o evento(w3schools)
 })
 
 let checarTurno = true;
+
 const jogadorX = "X";
 const jogadorO = "O";
+
+let jogX = [];
+let jogO = [];
+
+let emp = 0
 
 function jogoVelha(id) {
     const casa = document.getElementById(id)
@@ -28,11 +35,24 @@ function jogoVelha(id) {
     casa.classList.add(turno)
     casa.setAttribute("disabled", true)
     checarTurno = !checarTurno //alterar ao final o checarTurno para haver a mudança entre X e O
-    checarVencedor()
-}
 
-function checarVencedor() {
-    const vencedor = combGanhar.forEach(element => {
-        
-    });    
+    //armazenar botões pressionados
+    if (document.getElementById(id).className == "X"){
+        jogX.push(id)
+        console.log("Jogador X: ", jogX)
+    } else {
+        jogO.push(id)
+        console.log("Jogador O: ", jogO)
+    }
+
+    //definir vencedor
+    if (jogX.findIndex((i) => combGanhar[i])) {
+        console.log("teste")
+        emp = 0
+    }
+    //empate
+    emp++
+    if(emp == 9) {
+        alert("Deu Velha!!!")
+    }
 }

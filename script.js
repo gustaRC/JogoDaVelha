7b1c440194aa46b350d0f6bd25bb9785c801af5e
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-04-04 17:05:41 -0300

    Atualização 04/04
    Falta adicionar caso o Jogador O ganhe e resetar o jogo

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 const elementos = document.querySelectorAll("button")
 const casas = Array.from(elementos)
 
-const combGanhar = [
+let pontosX = document.getElementsByClassName("pontosX")
+let pontosO = document.getElementsByClassName("pontosO")
+
+const COMBINACOES = [
     ["n1", "n2", "n3"],
     ["n4", "n5", "n6"],
     ["n7", "n8", "n9"],
@@ -13,12 +16,11 @@ const combGanhar = [
     ["n3", "n5", "n7"]
 ]
 
-
 document.addEventListener("click", (el) => {
     jogoVelha(el.target.id) //target = retorna o elemento onde ocorreu o evento(w3schools)
 })
 
-let checarTurno = true;
+let checaTurno = true;
 
 const jogadorX = "X";
 const jogadorO = "O";
@@ -30,29 +32,60 @@ let emp = 0
 
 function jogoVelha(id) {
     const casa = document.getElementById(id)
-    turno = checarTurno ? jogadorX : jogadorO //se checarTurno for true jogadorX se nn jogadorO
+    turno = checaTurno ? jogadorX : jogadorO //se checarTurno for true jogadorX se nn jogadorO
     casa.textContent = turno
     casa.classList.add(turno)
     casa.setAttribute("disabled", true)
-    checarTurno = !checarTurno //alterar ao final o checarTurno para haver a mudança entre X e O
+    checaTurno = !checaTurno //alterar ao final o checarTurno para haver a mudança entre X e O
 
     //armazenar botões pressionados
     if (document.getElementById(id).className == "X"){
         jogX.push(id)
         console.log("Jogador X: ", jogX)
-    } else {
+    } else if (document.getElementById(id).className == "O") {
         jogO.push(id)
         console.log("Jogador O: ", jogO)
     }
 
-    //definir vencedor
-    if (jogX.findIndex((i) => combGanhar[i])) {
-        console.log("teste")
-        emp = 0
+    //definir vencedor - PLayer X
+    if (jogX.includes("n1") && jogX.includes("n2") && jogX.includes("n3")) {
+        venceu()
+        
+    } else if (jogX.includes("n4") && jogX.includes("n5") && jogX.includes("n6")) {
+        console.log("vencedor")
+    } else if (jogX.includes("n7") && jogX.includes("n8") && jogX.includes("n9")) {
+        console.log("vencedor")
+    } else if (jogX.includes("n1") && jogX.includes("n4") && jogX.includes("n7")) {
+        console.log("vencedor")
+    } else if (jogX.includes("n2") && jogX.includes("n5") && jogX.includes("n8")) {
+        console.log("vencedor")
+    } else if (jogX.includes("n3") && jogX.includes("n6") && jogX.includes("n9")) {
+        console.log("vencedor")
+    } else if (jogX.includes("n1") && jogX.includes("n5") && jogX.includes("n9")) {
+        console.log("vencedor")
+    } else if (jogX.includes("n3") && jogX.includes("n5") && jogX.includes("n7")) {
+        console.log("vencedor")
+    } else {
+        //empate
+        emp++
+        if(emp == 9) {
+            alert("Deu Velha!!!")
+        }
     }
-    //empate
-    emp++
-    if(emp == 9) {
-        alert("Deu Velha!!!")
+
+    function venceu() {
+        alert("Jogador " + turno + " Venceu! Parábens :)")
+
+        casa.innerHTML = " "
+        casa.classList.remove(turno)
+        jogX = []
+        jogO = []
+
+        casa.setAttribute("disabled", false)
+        
+        checaTurno = true
     }
+
+
+
 }
